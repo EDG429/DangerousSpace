50398966698b868c87ad79fbf243f5d06f1ae6f1
EDG429 <125379260+EDG429@users.noreply.github.com>
2024-11-15 22:03:06 +0100

    Expanded Primary Fire
    Sound: OK
    Muzzle Flash: Not OK

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -6,7 +6,11 @@ extends CharacterBody2D
 @export var SHOOTING_SPEED: float = 0.075 # Shooting cooldown in seconds
 @export var BULLET_PLAYER_PRIMARY_FIRE_scene: PackedScene = preload("res://Scenes/Player/bullet_player_primary_fire.tscn") # Getting the projectile scene for primary fire
 
-@onready var fire_timer: Timer = $Timer
+@onready var primary_firing_sound: AudioStreamPlayer2D = $Primary_FiringSound
+@onready var primary_fire_timer: Timer = $PrimaryFire_Timer
+@onready var primary_muzzle_light: PointLight2D = $Primary_MuzzleLight
+@onready var primary_fire_muzzle_flash_timer: Timer = $PrimaryFire_MuzzleFlash_Timer
+
 var can_fire: bool = true
 
 func _physics_process(delta: float) -> void:
@@ -30,23 +34,35 @@ func _physics_process(delta: float) -> void:
 	fire()
 
 func fire() -> void:
-	if Input.is_action_pressed("primary_fire") and can_fire:
-		can_fire = false
-		
-		 # Spawn a new projectile and add it to the scene
-		print("shots fired! Primary Fire")
-		var primary_fire_projectile = BULLET_PLAYER_PRIMARY_FIRE_scene.instantiate()
-		get_parent().add_child(primary_fire_projectile)
-		
-		# Set position and direction
-		primary_fire_projectile.global_position = global_position
-		primary_fire_projectile.direction = Vector2(0, 1)
-		
-		# Cooldown timer
-		fire_timer.start(SHOOTING_SPEED)
+	if Input.is_action_pressed("primary_fire") and can_fire: # Spawn a new projectile and add it to the scene
+		Primary_Fire()
 		
 	if Input.is_action_pressed("secondary_fire"):
 		print("shots fired! Secondary Fire")
 
-func _on_timer_timeout() -> void:
+func _on_timer_timeout_PrimaryFire() -> void:
 	can_fire = true
+	
+func _on_PrimaryFire_MuzzleFlashTimer_timeout() -> void:
+	# Disable the muzzle light when the timer times out
+	primary_muzzle_light.visible = false
+
+func Primary_Fire() -> void:
+	can_fire = false
+	print("shots fired! Primary Fire")		
+	# Enable Muzzle Light
+	primary_muzzle_light.global_position = global_position + Vector2(0, 24)
+	primary_muzzle_light.visible = true
+	primary_fire_muzzle_flash_timer.start()
+	
+	var primary_fire_projectile = BULLET_PLAYER_PRIMARY_FIRE_scene.instantiate()
+	get_parent().add_child(primary_fire_projectile)
+	
+	primary_firing_sound.play()
+	
+	# Set position and direction
+	primary_fire_projectile.global_position = global_position + Vector2(0, 25)
+	primary_fire_projectile.direction = Vector2(0, 1)
+		
+	# Cooldown timer
+	primary_fire_timer.start(SHOOTING_SPEED)
