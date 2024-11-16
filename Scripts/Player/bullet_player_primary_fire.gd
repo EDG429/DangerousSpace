a369815b98ef6abe3522a4cf057b542d68777d97
EDG429 <125379260+EDG429@users.noreply.github.com>
2024-11-16 20:50:35 +0100

    Prevented bullets from hitting player

--- Scripts/Player/bullet_player_primary_fire.gd
+++ Scripts/Player/bullet_player_primary_fire.gd
@@ -24,6 +24,6 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node) -> void:
 	# Handle collision with an enemy
-	if body.has_method("take_damage"):
+	if body.has_method("take_damage") and body.name != "Player":
 		body.take_damage(DAMAGE)
 		queue_free()  # Destroy the projectile
