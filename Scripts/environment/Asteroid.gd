b5cf3788fd8528ae9ef222a0ecdecf0746056990
EDG429 <125379260+EDG429@users.noreply.github.com>
2024-11-16 16:00:04 +0100

    Asteroid class implemented, take_damage() method successfully tested

---
+++ Scripts/environment/Asteroid.gd
@@ -0,0 +1,12 @@
+class_name Asteroid
+extends Sprite2D
+
+@export var damage: int = 10  # Default damage the asteroid deals
+
+func _on_Asteroid_body_entered(body: Node) -> void:
+	# Check if the body is the player and has a `take_damage` method
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+
+func take_damage() -> void:
+	pass
